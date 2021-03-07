e9746006dbdba214cbc3b58442c0ac9fd4caa880
Anastasia92613 <54165384+Anastasia92613@users.noreply.github.com>
2021-03-07 09:48:49 +0300

    Оживляем интерактивные элементы

---
+++ js/message.js
@@ -0,0 +1,60 @@
+const messageLink = document.querySelector(".button-contact-company");
+const messagePopup = document.querySelector(".form-letter");
+const messageClose = messagePopup.querySelector(".modal-close");
+const messageForm = messagePopup.querySelector(".message-form");
+const loginName = messagePopup.querySelector(".input-name-login");
+const loginEmail = messagePopup.querySelector(".input-name-login");
+const message = messagePopup.querySelector(".message-text");
+
+let isStorageSupport = true;
+let storage = "";
+
+try {
+    storage = localStorage.getItem("login");
+    storage = localStorage.getItem("email");
+} catch (err) {
+    isStorageSupport = false;
+}
+
+messageLink.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    messagePopup.classList.add("modal-show");
+
+});
+if (storage) {
+    loginName.value = storage;
+    loginEmail.value = storage;
+    message.focus();
+} else {
+    loginName.focus();
+
+}
+messageClose.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    messagePopup.classList.remove("modal-show");
+    messagePopup.classList.remove("modal-error");
+});
+
+messageForm.addEventListener("submit", function(evt) {
+    if (!loginName.value || !loginEmail.value || !message.value) {
+        evt.preventDefault();
+        messagePopup.classList.add("modal-error");
+        loginPopup.offsetWidth = loginPopup.offsetWidth;
+        messagePopup.classList.remove("modal-error");
+    } else {
+        if (isStorageSupport) {
+            localStorage.setItem("login", loginName.value);
+            localStorage.setItem("email", loginEmail.value);
+        }
+    }
+});
+
+window.addEventListener("keydown", function(evt) {
+    if (evt.keyCode === 27) {
+        if (messagePopup.classList.contains("modal-show")) {
+            evt.preventDefault();
+            messagePopup.classList.remove("modal-show");
+            messagePopup.classList.remove("modal-error");
+        }
+    }
+});
