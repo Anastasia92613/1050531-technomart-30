d0228dcf034c2ec1bcccbd000a920c7857e4647a
Anastasia92613 <54165384+Anastasia92613@users.noreply.github.com>
2021-03-09 09:54:29 +0300

    Защита личного проекта «Техномарт»

--- js/addProduct.js
+++ js/addProduct.js
@@ -1,12 +1,18 @@
-const buyProduct = document.querySelector(".buy");
+const buyProduct = document.querySelectorAll(".buy");
 const modalBuy = document.querySelector(".section-message");
 const modalClose = modalBuy.querySelector(".modal-close");
 
-
-buyProduct.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    modalBuy.classList.add("modal-show");
+buyProduct.forEach(function(button) {
+    button.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        modalBuy.classList.add("modal-show");
+    });
 });
+
+// buyProduct.addEventListener("click", function(evt) {
+//     evt.preventDefault();
+//     modalBuy.classList.add("modal-show");
+// });
 
 modalClose.addEventListener("click", function(evt) {
     evt.preventDefault();
